5cfdd03652b156eda5c1a594df87ddc4f6fc1e82
Von-kensington <vonkensington@gmail.com>
2025-03-04 21:51:00 -0600

    fix: remove temp files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,8 +3,8 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="about" options={{ title: 'About' }} />
+      <Tabs.Screen name="home" options={{ title: 'Home', headerShown: false }} />
+      <Tabs.Screen name="index" options={{ title: 'index' }} />
     </Tabs>
   );
 }
