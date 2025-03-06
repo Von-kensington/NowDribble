d211d9b93eb5658bdbeabe4bb3880518da07a7f8
Von-kensington <vonkensington@gmail.com>
2025-03-05 18:24:56 -0600

    feat: Added initial pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,8 +3,9 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ title: 'Home', headerShown: false }} />
-      <Tabs.Screen name="index" options={{ title: 'index' }} />
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="train" options={{ title: 'Train' }} />
+      <Tabs.Screen name="numbers" options={{ title: 'Numbers' }} />
     </Tabs>
   );
 }
---
+++ app/(tabs)/train.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Train() {
+  return <Text>Train</Text>
+}
