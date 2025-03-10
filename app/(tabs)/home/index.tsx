44cc3427052530a9af4d308e7f55862951b415d4
Von-kensington <vonkensington@gmail.com>
2025-03-09 20:26:53 -0500

    feat(home/index): home uses new videoplayer

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,92 +1,92 @@
 //import liraries
 import { Avatar, makeStyles, Text } from "@rneui/themed";
-import { View, TouchableOpacity, Modal } from "react-native";
-import { useVideoPlayer, VideoView } from 'expo-video';
+import { View, TouchableOpacity } from "react-native";
 
 import { Divider } from "@rneui/themed";
-import { useEvent } from "expo";
+import { useRouter } from "expo-router";
 const useStyles = makeStyles((theme) => ({
-    container: {
-        flex: 1,
-        backgroundColor: theme.colors.background,
-        padding: 16,
-    },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 18,
-    },
-    avatarStyle: {
-        backgroundColor: theme.colors.black,
-    },
-    contentContainer: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        opacity: 1,
-    },
-    video: {
-        width: "100%",
-        backgroundColor: theme.colors.black,
-        height: '30%',
-    },
-
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    padding: 16,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 18,
+  },
+  avatarStyle: {
+    backgroundColor: theme.colors.black,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    opacity: 1,
+  },
+  video: {
+    width: "100%",
+    backgroundColor: theme.colors.black,
+    height: "30%",
+  },
 }));
 const videoSource =
-    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 const Home = () => {
-    const styles = useStyles();
-    const player = useVideoPlayer(videoSource, player => { });
+  const styles = useStyles();
+  const router = useRouter();
+  return (
+    <View style={styles.container}>
+      {/* Profile Header */}
+      <View style={styles.header}>
+        <Avatar
+          rounded
+          icon={{ name: "user", type: "feather" }}
+          size={50}
+          containerStyle={styles.avatarStyle}
+        />
+        <Text style={{ fontSize: 18, fontWeight: "bold", marginLeft: 8 }}>
+          Coach Cook
+        </Text>
+      </View>
 
-    const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
-    return (
-        <View style={styles.container}>
-            {/* Profile Header */}
-            <View style={styles.header}>
-                <Avatar
-                    rounded
-                    icon={{ name: "user", type: "feather" }}
-                    size={50}
-                    containerStyle={styles.avatarStyle}
+      {/* Welcome Message */}
+      <Text style={{ fontSize: 16, marginBottom: 10 }}>
+        Welcome to Now Dribble!
+      </Text>
 
-                />
-                <Text style={{ fontSize: 18, fontWeight: "bold", marginLeft: 8 }}>
-                    Coach Cook
-                </Text>
-            </View>
+      {/* Watch Intro Video Button */}
+      <TouchableOpacity
+        onPress={() =>
+          router.push({
+            pathname: "../VideoPlayer",
+            params: { url: videoSource },
+          })
+        }
+        style={{ padding: 10 }}
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "#E91E63",
+            textAlign: "center",
+            textDecorationLine: "underline",
+          }}
+        >
+          Watch Intro Video
+        </Text>
+      </TouchableOpacity>
 
-            {/* Welcome Message */}
-            <Text style={{ fontSize: 16, marginBottom: 10 }}>Welcome to Now Dribble!</Text>
+      {/* Divider */}
+      <Divider style={{ marginVertical: 16 }} />
 
-            {/* Watch Intro Video Button */}
-            <TouchableOpacity onPress={() => player.play()} style={{ padding: 10 }}>
-                <Text style={{
-                    fontSize: 16,
-                    fontWeight: "bold",
-                    color: "#E91E63",
-                    textAlign: "center",
-                    textDecorationLine: "underline",
-                }}>
-                    Watch Intro Video
-                </Text>
-            </TouchableOpacity>
-
-            {/* Divider */}
-            <Divider style={{ marginVertical: 16 }} />
-
-            {/* Empty State Message */}
-            <Text style={{ fontSize: 16, textAlign: "center" }}>
-                Nothing has been posted here.
-            </Text>
-            <Modal animationType="slide" transparent visible={isPlaying}>
-                <View style={styles.contentContainer}>
-                    <VideoView contentFit="contain" style={styles.video} player={player} allowsFullscreen={false} />
-                </View>
-            </Modal>
-        </View>
-    );
+      {/* Empty State Message */}
+      <Text style={{ fontSize: 16, textAlign: "center" }}>
+        Nothing has been posted here.
+      </Text>
+    </View>
+  );
 };
-
-
 
 export default Home;
