af25dc486e98bbc47cdc1671501b07a165d15ceb
Von-kensington <vonkensington@gmail.com>
2025-03-09 20:47:37 -0500

    feat(root): added app root page

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,9 @@
-//import liraries
-import { Redirect } from 'expo-router';
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Redirect } from "expo-router";
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-// create a component
-const Home = () => {
-    return (
-        <Redirect href="./home" />
-    );
+const App = () => {
+  return <Redirect href="./home" />;
 };
 
-//make this component available to the app
-export default Home;
+export default App;
