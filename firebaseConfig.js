6d481650003423e4233ea7db8ca25d398f4613dc
Von-kensington <vonkensington@gmail.com>
2025-03-14 21:29:49 -0500

    feat(firebase-config): initialize auth in firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-// Optionally import the services that you want to use
-// import {...} from 'firebase/auth';
-// import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';
-// import {...} from 'firebase/functions';
-// import {...} from 'firebase/storage';
-
-// Initialize Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDW5tFADQd0Ocuhf_WzMGkmK48JUwQJjP8",
     authDomain: "nowdribble-ac527.firebaseapp.com",
@@ -20,5 +13,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+export const auth = getAuth(app);
